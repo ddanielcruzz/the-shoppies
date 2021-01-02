0ce2effd26b4dc71a08af6b2db95915fc74c7749
Daniel Cruz <nox.sb@hotmail.com>
2021-01-02 17:56:07 -0600

    Improved readability

--- src/components/MovieResults/MovieResults.tsx
+++ src/components/MovieResults/MovieResults.tsx
@@ -15,6 +15,21 @@ export const MovieResults = ({
   setNominatedMovies,
   setMovies,
 }: MovieResultsProps) => {
+  const handleNomination = (movie: Movie) => {
+    setMovies(
+      movies.map((outMovie) => {
+        if (outMovie.imdbID === movie.imdbID) {
+          return {
+            ...outMovie,
+            isNominated: true,
+          };
+        }
+        return outMovie;
+      })
+    );
+    setNominatedMovies((prev) => [...prev, { ...movie, isNominated: true }]);
+  };
+
   return (
     <section className={appStyles.container}>
       <h3>Results for {movieTitle}</h3>
@@ -26,23 +41,7 @@ export const MovieResults = ({
               <h2>{Title}</h2> <p>{Year}</p>
               <button
                 disabled={isNominated}
-                onClick={() => {
-                  setMovies(
-                    movies.map((outMovie) => {
-                      if (outMovie.imdbID === movie.imdbID) {
-                        return {
-                          ...outMovie,
-                          isNominated: true,
-                        };
-                      }
-                      return outMovie;
-                    })
-                  );
-                  setNominatedMovies((prev) => [
-                    ...prev,
-                    { ...movie, isNominated: true },
-                  ]);
-                }}
+                onClick={() => handleNomination(movie)}
               >
                 Nominate
               </button>
