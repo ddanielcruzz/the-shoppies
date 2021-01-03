4aaa373407de5916b2eb5b23958a23c520ea9095
Daniel Cruz <nox.sb@hotmail.com>
2021-01-03 00:14:23 -0600

    Added confetti effect

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./App.module.css";
 import { QueryFunction, QueryFunctionContext, useQuery } from "react-query";
 import { useDebounce } from "./lib/hooks/useDebounce";
 import { MovieResults, NominatedMovies } from "./components";
+import confetti from "canvas-confetti";
 
 export interface Movie {
   Poster: string;
@@ -43,6 +44,44 @@ function App() {
   const [nominatedMovies, setNominatedMovies] = useState<Movie[]>([]);
   const [page, setPage] = useState(1);
   const debouncedMovieTitle = useDebounce(movieTitle, 1000);
+
+  useEffect(() => {
+    if (nominatedMovies.length > 4) {
+      const duration = 5 * 1000;
+      const animationEnd = Date.now() + duration;
+      const defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+
+      const randomInRange = (min: number, max: number) => {
+        return Math.random() * (max - min) + min;
+      };
+
+      const interval: NodeJS.Timeout = setInterval(function () {
+        const timeLeft = animationEnd - Date.now();
+
+        if (timeLeft <= 0) {
+          return clearInterval(interval);
+        }
+
+        const particleCount = 50 * (timeLeft / duration);
+        // since particles fall down, start a bit higher than random
+        confetti(
+          Object.assign({}, defaults, {
+            particleCount,
+            origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 },
+          })
+        );
+        confetti(
+          Object.assign({}, defaults, {
+            particleCount,
+            origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 },
+          })
+        );
+      }, 250);
+      return () => {
+        confetti.reset();
+      };
+    }
+  }, [nominatedMovies]);
 
   const { data } = useQuery<unknown, unknown, QueryResponse>(
     ["movies", debouncedMovieTitle, page],
